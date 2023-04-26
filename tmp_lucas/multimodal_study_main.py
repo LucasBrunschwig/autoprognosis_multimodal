c1e1005a917d9d3ef0b8120a8912eff1f2f5bb24
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-04-26 12:07:58 +0200

    Update: updated data loading to be consistent with the data loader modifications

--- tmp_lucas/multimodal_study_main.py
+++ tmp_lucas/multimodal_study_main.py
@@ -13,21 +13,25 @@ from loader import DataLoader
 # autoprognosis absolute
 from autoprognosis.studies.multimodal_classifier import MultimodalStudy
 
+# Load Data
 DL = DataLoader(
-    path_="/home/enwoute/Documents/master-thesis/data/pad-ufe-20", data_src_="PAD-UFES"
+    path_="/home/enwoute/Documents/master-thesis/data/pad-ufe-20",
+    data_src_="PAD-UFES",
+    format_="PIL",
 )
+DL.load_dataset()
 
-
-ids, X_images, X_clinic, Y = DL.load_dataset()
-
+# Sample Dataset for Testing Purpose
+X_images, X_clinic, Y = DL.sample_dataset(n=50)
 df = X_images.join((X_clinic, Y))
 
+# Study Name
 study_name = "first test"
 study = MultimodalStudy(
     study_name=study_name,
     dataset=df,  # pandas DataFrame
     target="label",  # the label column in the dataset
-    image="images",
+    image="images",  # tje images column in the dataset
 )
 
 model = study.fit()
