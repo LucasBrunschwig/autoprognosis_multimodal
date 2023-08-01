da958642008732f1935b6bf6c4ff65b059057f16
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-08-01 16:27:33 +0200

    updated: improved the results output.A plot function to show results directly. Still requires some work on different number of channels and the get_image_model() in generators.py

--- src/autoprognosis/plugins/explainers/plugin_grad_cam.py
+++ src/autoprognosis/plugins/explainers/plugin_grad_cam.py
@@ -4,12 +4,14 @@ from typing import Any, List, Optional
 
 # third party
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # autoprognosis absolute
 from autoprognosis.plugins.explainers.base import ExplainerPlugin
+from autoprognosis.utils.default_modalities import IMAGE_KEY
 from autoprognosis.utils.pip import install
 
 for retry in range(2):
@@ -24,22 +26,58 @@ for retry in range(2):
         install(depends)
 
 
+def get_last_conv_layer_before_classifier(model, input_size):
+
+    model.eval()  # Set the model to evaluation mode
+    model.cpu()  # Set the model to cpu if it was trained on cuda
+
+    # Register hooks to store activations in each layer
+    activations = {}
+
+    def hook_fn(module_name):
+        def hook(module, input, output):
+            activations[module_name] = output
+
+        return hook
+
+    hooks = []
+    for name, module in model.named_modules():
+        if (
+            isinstance(module, nn.Conv2d)
+            or isinstance(module, nn.AdaptiveAvgPool2d)
+            or isinstance(module, nn.AvgPool2d)
+        ):
+            hooks.append(module.register_forward_hook(hook_fn(name)))
+
+    # Forward pass to collect activations
+    input_tensor = torch.rand(*input_size).unsqueeze(0)
+    with torch.no_grad():
+
+        model(input_tensor)
+
+    # Remove hooks
+    for hook in hooks:
+        hook.remove()
+
+    # Find the last convolutional layer before the classifier
+    last_conv_layer = list(activations.keys())[-1]
+
+    return last_conv_layer
+
+
 class GradCAM:
     def __init__(
         self,
         estimator: Any,
-        target_layer: str,
     ):
 
         self.plugin = estimator.stages[-1]
         self.model = self.plugin.get_model()
-        self.target_layer = target_layer
 
         self.gradient = None
         self.activations = None
         self.handles = None
-
-        self.register_hooks()
+        self.target_layer = None
 
     def register_hooks(
         self,
@@ -61,7 +99,10 @@ class GradCAM:
 
         self.handles = [backward_handle, forward_handle]
 
-    def generate_cam(self, input_image, target_class):
+    def generate_cam(self, input_image, target_class, target_layer):
+
+        self.target_layer = target_layer
+        self.register_hooks()
         self.model.zero_grad()
 
         output = self.plugin.predict_proba_tensor(input_image)
@@ -117,6 +158,8 @@ class GradCAMPlugin(ExplainerPlugin):
         >>> explainer = Explainers().get(
         >>>     "grad_cam",
         >>>     model,
+        >>>     X_train,
+        >>>     y_train,
         >>>     task_type="classification",
         >>> )
         >>>
@@ -128,7 +171,6 @@ class GradCAMPlugin(ExplainerPlugin):
         estimator: Any,
         X: pd.DataFrame,
         y: pd.DataFrame,
-        target_layer: str,
         images: Optional[List] = None,
         task_type: str = "classification",
         prefit: bool = False,
@@ -156,11 +198,24 @@ class GradCAMPlugin(ExplainerPlugin):
                 raise RuntimeError("invalid input for risk estimation interpretability")
 
         if task_type == "classification":
-            self.explainer = GradCAM(estimator, target_layer=target_layer)
+            self.explainer = GradCAM(estimator)
         else:
             raise ValueError("Not Implemented")
 
-    def explain(self, X: pd.DataFrame, label: pd.DataFrame) -> pd.DataFrame:
+    def explain(
+        self, X: pd.DataFrame, label: pd.DataFrame, n_top=1, target_layer: str = None
+    ) -> dict:
+
+        if target_layer is None:
+            # TODO: create a function in generator if modality type == image or multimodal ->
+            #  function get_model() self.estimator.stages[-1].get_image_model()
+            # by default grad-cam will extract the last convolutional layer
+            input_size = self.estimator.stages[-1].get_size()
+            n_channel = 3
+            input_size = (n_channel, input_size, input_size)
+            target_layer = get_last_conv_layer_before_classifier(
+                self.estimator.stages[-1].get_model(), input_size
+            )
 
         label = pd.DataFrame(LabelEncoder().fit_transform(label))
 
@@ -171,7 +226,7 @@ class GradCAMPlugin(ExplainerPlugin):
         predictions["label"] = label.squeeze()
         predictions["proba"] = predictions.apply(lambda d: d[d["label"]], axis=1)
         predictions = predictions[["proba", "label"]]
-        indices = predictions.groupby("label")["proba"].nlargest(3)
+        indices = predictions.groupby("label")["proba"].nlargest(n_top)
 
         for label_, ix in indices.index:
             local_X = X.iloc[ix]
@@ -182,7 +237,7 @@ class GradCAMPlugin(ExplainerPlugin):
                 local_X = stage.transform(local_X)
 
             # Generate CAM
-            cam_normalized = self.explainer.generate_cam(local_X, label_)
+            cam_normalized = self.explainer.generate_cam(local_X, label_, target_layer)
             cam_normalized = cv2.applyColorMap(
                 np.uint8(255 * cam_normalized), cv2.COLORMAP_JET
             )
@@ -192,6 +247,80 @@ class GradCAMPlugin(ExplainerPlugin):
             results[label_].append([img_array / 255.0, superposed_image / 255.0])
 
         return results
+
+    def plot(
+        self,
+        X: pd.DataFrame,
+        label: pd.DataFrame,
+        target_layer: str = None,
+        class_names: list = None,
+    ) -> dict:
+
+        if target_layer is None:
+            # TODO: create a function in generator if modality type == image or multimodal ->
+            #  function get_model() self.estimator.stages[-1].get_image_model()
+            # by default grad-cam will extract the last convolutional layer
+            input_size = self.estimator.stages[-1].get_size()
+            n_channels = 3
+            input_size = (n_channels, input_size, input_size)
+            target_layer = get_last_conv_layer_before_classifier(
+                self.estimator.stages[-1].get_model(), input_size
+            )
+
+        label = pd.DataFrame(LabelEncoder().fit_transform(label))
+
+        results = {label_: [] for label_ in label.squeeze().unique()}
+
+        # Get the top 2 highest score for each class
+        predictions = pd.DataFrame(self.estimator.predict_proba(X))
+        predictions["label"] = label.squeeze()
+        predictions["proba"] = predictions.apply(lambda d: d[d["label"]], axis=1)
+        predictions = predictions[["proba", "label"]]
+        indices = predictions.groupby("label")["proba"].nlargest(1)
+
+        for label_, ix in indices.index:
+            local_X = X.iloc[ix]
+
+            # Preprocess Data
+            local_X = pd.DataFrame(local_X)
+            for stage in self.estimator.stages[:-1]:
+                local_X = stage.transform(local_X)
+
+            # Generate CAM
+            cam_normalized = self.explainer.generate_cam(local_X, label_, target_layer)
+            cam_normalized = cv2.applyColorMap(
+                np.uint8(255 * cam_normalized), cv2.COLORMAP_JET
+            )
+            img_array = np.array(local_X.squeeze())
+            alpha = 0.4
+            superposed_image = alpha * cam_normalized + (1 - alpha) * img_array
+            results[label_].append([img_array / 255.0, superposed_image / 255.0])
+
+        fig, axes = plt.subplots(2, len(results), figsize=(6 * len(results), 12))
+        for label, images in results.items():
+            image = images[0]
+            if class_names is not None:
+                axes[0, label].set_title(class_names[label], fontsize=17)
+
+            axes[0, label].imshow(image[0])
+            axes[0, label].axis("off")
+
+            axes[1, label].imshow(image[1])
+            axes[1, label].axis("off")
+
+        axes[0, 0].get_yaxis().set_visible(False)
+        axes[1, 0].get_yaxis().set_visible(False)
+
+        # Add row names to the second row
+        fig.text(0.06, 0.7, "Original", ha="center", va="center", fontsize=17)
+        fig.text(0.06, 0.27, "Grad-CAM", ha="center", va="center", fontsize=17)
+
+        plt.subplots_adjust(wspace=0.3, hspace=0.3)
+
+        return results
+
+    def modality_type(self):
+        return IMAGE_KEY
 
     @staticmethod
     def name() -> str:
