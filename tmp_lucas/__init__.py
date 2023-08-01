0e2310db5b3037464855f33398499e7dca5ba832
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-08-01 16:49:52 +0200

    bugfix: relative import

--- tmp_lucas/__init__.py
+++ tmp_lucas/__init__.py
@@ -1,10 +1,6 @@
-# third party
-from build_pipeline import (
-    build_intermediate_fusion_from_dict,
-    build_late_fusion_from_dict,
-)
-from loader import DataLoader
+# autoprognosis relative
+from .build_pipeline import build_late_fusion_from_dict
+from .loader import DataLoader
 
 DataLoader = DataLoader
 build_late_fusion_from_dict = build_late_fusion_from_dict
-build_intermediate_fusion_from_dict = build_intermediate_fusion_from_dict
