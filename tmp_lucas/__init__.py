8683bbca926de61c9298d1e216c20c252a192393
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-08-01 16:38:43 +0200

    added: init file to import necessary from outside folder

---
+++ tmp_lucas/__init__.py
@@ -0,0 +1,10 @@
+# third party
+from build_pipeline import (
+    build_intermediate_fusion_from_dict,
+    build_late_fusion_from_dict,
+)
+from loader import DataLoader
+
+DataLoader = DataLoader
+build_late_fusion_from_dict = build_late_fusion_from_dict
+build_intermediate_fusion_from_dict = build_intermediate_fusion_from_dict
