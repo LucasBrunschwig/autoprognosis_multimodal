8d6bb4208c9da52cc4c8c09dc0e7217f6424a4d9
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-08-01 16:34:22 +0200

    added: possibility to transform your dataframe into a suitable format for multimodal fitting

--- src/autoprognosis/utils/default_modalities.py
+++ src/autoprognosis/utils/default_modalities.py
@@ -1,2 +1,36 @@
+# stdlib
+from typing import Optional
+
+# third party
+import pandas as pd
+
 IMAGE_KEY = "img"
 TABULAR_KEY = "tab"
+MULTIMODAL_KEY = "multimodal"
+
+
+def dataset_to_multimodal(
+    dataset: pd.DataFrame,
+    label: str,
+    image: list,
+    tabular: Optional[list] = [],
+):
+
+    dataset = dataset.copy(deep=True)
+
+    if label in dataset.columns:
+        df_label = dataset[[label]].squeeze()
+        dataset.drop([label], axis=1, inplace=True)
+    else:
+        raise ValueError("Target not in dataset")
+
+    X = {IMAGE_KEY: [], TABULAR_KEY: []}
+    if not isinstance(image, list):
+        image = [image]
+    X[IMAGE_KEY] = dataset[image]
+
+    if len(tabular) == 0:
+        tabular = dataset.columns.difference(image)
+    X[TABULAR_KEY] = dataset[tabular]
+
+    return X, df_label
