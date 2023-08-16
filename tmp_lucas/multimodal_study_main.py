1b3330eaf3f43c2923626a523a3e438ec31441c3
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-08-16 04:21:17 +0200

    update: new parameters

--- tmp_lucas/multimodal_study_main.py
+++ tmp_lucas/multimodal_study_main.py
@@ -22,24 +22,7 @@ os.environ["N_OPT_JOBS"] = "1"
 
 logger.debug("Loading Images")
 
-
-def worker_dataloader(state):
-    # Load Data
-    DL = DataLoader(
-        path_="/home/enwoute/Documents/master-thesis/data/pad-ufe-20",
-        data_src_="PAD-UFES",
-        format_="PIL",
-    )
-    DL.load_dataset(classes=["ACK", "BCC"])
-
-    # Sample Dataset for Testing Purpose
-    state["X_images"], state["X_clinic"], state["Y"] = DL.sample_dataset(state["n"])
-
-
 if __name__ == "__main__":
-
-    subprocess = False
-    n = 600
 
     logger.info(
         f"GB available before loading data: {psutil.virtual_memory().available/1073741824:.2f}"
@@ -50,8 +33,7 @@ if __name__ == "__main__":
         data_src_="PAD-UFES",
         format_="PIL",
     )
-    df = DL.load_dataset()
-    df = DL.sample_dataset(n)
+    df = DL.load_dataset(sample=False)
 
     logger.info("Image Loaded")
     logger.info(
@@ -68,7 +50,9 @@ if __name__ == "__main__":
         image="image",  # the image column in the dataset
         multimodal_type=multimodal_type,
         sample_for_search=False,  # no Sampling
-        classifiers=["neural_nets", "random_forest", "lgbm"],
+        classifiers=["neural_nets"],
+        image_classifiers=["cnn_fine_tune"],
+        predefined_cnn=["alexnet"],
         n_folds_cv=5,
         num_iter=1,
         timeout=3600,
