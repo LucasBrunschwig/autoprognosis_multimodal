582defc0d384fe6721d285956e55681d305a123d
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-09-28 16:49:06 +0200

    new file: custom dataset from Pytorch to work with image dataset and on-the-fly data augmentation

--- src/autoprognosis/plugins/utils/custom_dataset.py
+++ src/autoprognosis/plugins/utils/custom_dataset.py
@@ -1,7 +1,11 @@
+# stdlib
+from typing import Union
+
 # third party
 import pandas as pd
 
 # autoprognosis absolute
+import autoprognosis.logger as log
 from autoprognosis.utils.pip import install
 
 for retry in range(2):
@@ -19,11 +23,46 @@ for retry in range(2):
     try:
         # third party
         import torchvision
+        from torchvision import transforms
 
         break
     except ImportError:
         depends = ["torchvision"]
         install(depends)
+
+DATA_AUGMENTATION = {
+    "": None,
+    "autoaugment_imagenet": [
+        transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET)
+    ],
+    "autoaugment_cifar10": [
+        transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10)
+    ],
+    "rand_augment": [transforms.RandAugment()],
+    "trivial_augment": [transforms.TrivialAugmentWide()],
+    "simple_strategy": [
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomRotation(10),
+    ],
+    "gaussian_noise": [
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomRotation(10),
+        transforms.GaussianBlur(3, sigma=(0.1, 0.5)),
+    ],
+    "color_jittering": [
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05),
+    ],
+}
+
+data_augmentation_strategies = list(DATA_AUGMENTATION.keys())
 
 
 class TrainingImageDataset(Dataset):
@@ -83,3 +122,25 @@ class TestImageDataset(Dataset):
         image = self.preprocess(image)
 
         return image
+
+
+def build_data_augmentation_strategy(
+    data_augmentation_: Union[str, transforms.Compose, None] = None
+) -> Union[torchvision.transforms.Compose, None]:
+    """This function returns the selected data augmentation strategy. The data augmentation can be either a custom
+    composition of transforms, one of the predefined strategy in the data augmentation dictionary, or none.
+    """
+    if isinstance(data_augmentation_, str):
+        if data_augmentation_ == "":
+            return None
+        elif data_augmentation_ in DATA_AUGMENTATION.keys():
+            return transforms.Compose(DATA_AUGMENTATION[data_augmentation_])
+        else:
+            raise ValueError(
+                f"Unknown Data Augmentation Strategy: {data_augmentation_}"
+            )
+    elif isinstance(data_augmentation_, transforms.Compose):
+        return data_augmentation_
+    else:
+        log.warning("Data Augmentation set to None")
+        return None
