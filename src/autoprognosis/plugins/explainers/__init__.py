faee0d0bf725c3349314c3d621519c8fd953317f
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-10-17 11:44:33 +0200

    Explainers() has an optional argument to return explainers for different model

--- src/autoprognosis/plugins/explainers/__init__.py
+++ src/autoprognosis/plugins/explainers/__init__.py
@@ -8,15 +8,29 @@ from autoprognosis.plugins.core.base_plugin import PluginLoader
 # autoprognosis relative
 from .base import ExplainerPlugin  # noqa: F401,E402
 
-plugins = glob.glob(join(dirname(__file__), "plugin*.py"))
+plugins_tabular = glob.glob(join(dirname(__file__), "tabular/plugin*.py"))
+plugins_image = glob.glob(join(dirname(__file__), "image/plugin*.py"))
+plugins_multimodal = glob.glob(join(dirname(__file__), "image/plugin*.py"))
+
+plugins_all = plugins_tabular + plugins_image + plugins_multimodal
 
 
 class Explainers(PluginLoader):
-    def __init__(self) -> None:
+    def __init__(self, data_type=None) -> None:
+
+        if data_type == "multimodal":
+            plugins = plugins_multimodal
+        elif data_type == "image":
+            plugins = plugins_image
+        elif data_type == "tabular":
+            plugins = plugins_tabular
+        else:
+            plugins = plugins_all
+
         super().__init__(plugins, ExplainerPlugin)
 
 
-__all__ = [basename(f)[:-3] for f in plugins if isfile(f)] + [
+__all__ = [basename(f)[:-3] for f in plugins_all if isfile(f)] + [
     "Explainers",
     "ExplainerPlugin",
 ]
