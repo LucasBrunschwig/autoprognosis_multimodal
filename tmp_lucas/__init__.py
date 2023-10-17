b25fc1b33ddbd4cd4d6b3e14ad18ae2112eed7d5
LucasBrunschwig <lucas.brunschwig@hotmail.fr>
2023-10-17 22:35:57 +0200

    removed unused files

--- tmp_lucas/__init__.py
+++
@@ -1,6 +0,0 @@
-# autoprognosis relative
-from .build_pipeline import build_late_fusion_from_dict
-from .loader import DataLoader
-
-DataLoader = DataLoader
-build_late_fusion_from_dict = build_late_fusion_from_dict
